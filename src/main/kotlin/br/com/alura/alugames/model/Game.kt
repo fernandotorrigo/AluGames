075ab3751321c41a490dd0ed80061d2011cc4469
Fernando Torrigo <torrigofernando@gmail.com>
2024-03-13 19:32:07 +0000

    fix code format

--- src/main/kotlin/br/com/alura/alugames/model/Game.kt
+++ src/main/kotlin/br/com/alura/alugames/model/Game.kt
@@ -1,17 +1,23 @@
 package br.com.alura.alugames.model
 
 data class Game(
-    val titulo: String, val capa: String
+    val titulo: String,
+    val capa: String
 ) {
     var description: String? = null
     var price = 0.0
 
-    constructor(titulo: String, capa: String, price: Double, description: String) : this(titulo, capa) {
+    constructor(titulo: String, capa: String, price: Double, description: String) :
+            this(titulo, capa) {
         this.price = price
         this.description = description
     }
 
     override fun toString(): String {
-        return "Meu Jogo: \n" + "Título: $titulo \n" + "Capa: $capa \n" + "Descricao: $description" + "Preço: $price"
+        return "Meu Jogo: \n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $description \n" +
+                "Preço: $price"
     }
 }
